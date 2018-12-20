02377234d92ae42930125d39c56bc775266124e0
danlaunch <35396771+danlaunch@users.noreply.github.com>
2018-12-21 00:38:14 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -487,7 +487,7 @@ const defaultStyles = StyleSheet.create({
         borderLeftColor: 'transparent',
         width: 0,
         height: 0,
-        top: 20,
+        top: 30,
         right: 10,
     },
     modalViewTop: {
